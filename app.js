980321273b1086818f9b2cbb2a3908055d9cc321
DanishOnGit <youcandoit1911@gmail.com>
2021-02-06 19:57:28 +0530

    feat: added genre and playlists

--- app.js
+++ app.js
@@ -1,14 +1,48 @@
+let id=myid;
+let key=token;
 async function getToken(){
+    let response;
+    
+   try{
+    console.log("hello")
+     response= await fetch("https://accounts.spotify.com/api/token",{
+        method:"POST",
+        headers:{
+            "Content-Type": "application/x-www-form-urlencoded",
+            
+            "Authorization":"Basic " + btoa(id +":"+ key)
+            
+        },
+        body:"grant_type=client_credentials"
+            
+    }) ;
+   } catch(err){
+       console.log(err)
+   }
+   const data= await response.json();
+   return data.access_token
 
-const response= await fetch("https://accounts.spotify.com/api/token",{
-    method:'POST',
-    headers:{
-        "Content-Type": 'application/x-www-form-urlencoded',
-        "Authorization":"Basic"
-        
-    },
-    body:"grant_type=client_credentials"
-        
-});
-return (await response).json();
+}getToken();
+
+async function getGenres(token){
+    const result=await fetch("https://api.spotify.com/v1/browse/categories",{
+        method:"GET",
+        headers:{
+            "Authorization":"Bearer "+token
+        }
+
+    });
+    const data=await result.json();
+    return data.categories.items
 }
+async function getPlaylists(token,genreId){
+    const result=await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,{
+        method:"GET",
+        headers:{
+            "Authorization":"Bearer "+token
+        }
+    });
+    const data=await result.json();
+    return data.playlists.items
+}
+async function getTracks(token,)
