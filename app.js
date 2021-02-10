07b1ce2daffbe13accc0601fb0af555424f428a0
DanishOnGit <youcandoit1911@gmail.com>
2021-02-10 23:06:43 +0530

    refactor apiresposnecontrol

--- app.js
+++ app.js
@@ -3,55 +3,72 @@ let playlistSelect=document.querySelector(".playlists");
 
 let id=myid;
 let key=token;
-var access;
-async function getToken(){
-    let response;
-   try{
-    console.log("hello")
-      response= await fetch("https://accounts.spotify.com/api/token",{
-        method:"POST",
-        headers:{
-            "Content-Type": "application/x-www-form-urlencoded",
-            
-            "Authorization":"Basic " + btoa(id +":"+ key)
-            
-        },
-        body:"grant_type=client_credentials"
-            
-    }) ;
-   } catch(err){
-       console.log(err)
-   }
-   const data= await response.json();
-   console.log(data)
-   access=data.access_token
-   return data.access_token
+const ApiResponseControl=(function(){
 
-} getToken();
-
-async function getGenres(token){
-    let result;
+    const getToken=async()=>{
+        let response;
+       try{
+        console.log("hello")
+          response= await fetch("https://accounts.spotify.com/api/token",{
+            method:"POST",
+            headers:{
+                "Content-Type": "application/x-www-form-urlencoded",
+                
+                "Authorization":"Basic " + btoa(id +":"+ key)
+                
+            },
+            body:"grant_type=client_credentials"
+                
+        }) ;
+       } catch(err){
+           console.log(err)
+       }
+       const data= response.json();
+       console.log(data)
+      
+       return  data.access_token
     
-    try{
-         result=await fetch("https://api.spotify.com/v1/browse/categories",{
-        method:"GET",
-        headers:{
-            "Authorization":"Bearer "+ token
+    } 
+    
+    const getGenres=async(token)=>{
+        let result;
+        
+        try{
+             result=await fetch("https://api.spotify.com/v1/browse/categories",{
+            method:"GET",
+            headers:{
+                "Authorization":"Bearer "+ token
+            }
+    
+        });
         }
+        catch(err){
+            console.log(err)
+        }
+        
+        const data=await result.json();
+        console.log(data);
+        console.log(data.categories.items)
+        return data.categories.items
+    }
+  
 
-    });
+    return {
+        _getToken(){
+            return getToken()
+        },
+        _getGenre(token){
+            return getGenres(token)
+        }
     }
-    catch(err){
-        console.log(err)
-    }
-    
-    const data=await result.json();
-    console.log(data);
-    console.log(data.categories.items)
-    return data.categories.items
-}
-console.log(access);
-getGenres(access);
+}) ();
+console.log(ApiResponseControl._getToken)
+
+
+
+
+
+
 // async function getPlaylists(token,genreId){
 //     const result=await fetch(`https://api.spotify.com/v1/browse/categories/${genreId}/playlists?limit=${limit}`,{
 //         method:"GET",
